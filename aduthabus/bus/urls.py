5d6d8cb733e68d5454a0647907ad4f91e6767754
abdulagfar <agfar.info@gmail.com>
2019-04-17 22:05:03 +0530

    added front end for login page

--- aduthabus/bus/urls.py
+++ aduthabus/bus/urls.py
@@ -7,5 +7,6 @@ from django.conf.urls.static import static
 app_name = 'bus'
 urlpatterns = [
 	path('login/', views.redirect_to_login, name='redirect_to_login'),
-	path('admin-login/', views.admin_login, name='admin_login')
+	path('admin-login/', views.admin_login, name='admin_login'),
+	
 ]
