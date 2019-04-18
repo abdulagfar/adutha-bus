8709eec151bb0d1fa38912fc80f08282758f5815
SauravManoj <sauravmanoj97@gmail.com>
2019-04-18 16:34:19 +0530

    formatted code

--- aduthabus/bus/urls.py
+++ aduthabus/bus/urls.py
@@ -5,12 +5,12 @@ from . import views
 
 app_name = 'bus'
 urlpatterns = [
-	path('', views.home_page, name='home_page'),
-	path('login/', views.redirect_to_login, name='redirect_to_login'),
-	path('add-data/', views.admin_login, name='admin_login'),
-	path('added/', views.add_data, name='add_data'),
-	path('login/', views.admin_logout, name='admin_logout'),
-	path('table/', views.show_table),
-	path('home/', views.home_page),
-	path("bus_details/<int:id>/", views.bus_details),
+    path('', views.home_page, name='home_page'),
+    path('login/', views.redirect_to_login, name='redirect_to_login'),
+    path('add-data/', views.admin_login, name='admin_login'),
+    path('added/', views.add_data, name='add_data'),
+    path('login/', views.admin_logout, name='admin_logout'),
+    path('table/', views.show_table),
+    path('home/', views.home_page),
+    path("bus_details/<int:id>/", views.bus_details),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- aduthabus/bus/views.py
+++ aduthabus/bus/views.py
@@ -1,19 +1,19 @@
 from django.shortcuts import render
 from .models import bus_timetable
-from collections import OrderedDict 
+from collections import OrderedDict
 from django.http import Http404
 
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth import authenticate, login, logout
-  
+
 
 def home_page(request):
 
-    if request.method=='POST':
+    if request.method == 'POST':
 
-        source=request.POST.get("q_source")
-        destination=request.POST.get("q_destination")
+        source = request.POST.get("q_source")
+        destination = request.POST.get("q_destination")
 
         if source == '' or destination == '':
             context = {'unavailable_msg': 'Enter source/destination.'}
@@ -23,62 +23,62 @@ def home_page(request):
 
     return render(request, "home.html")
 
+
 def show_table(request, q_s, q_d, *args, **kargs):
-    d=[]
-  
-    all_roots=bus_timetable.objects.all()
+    d = []
 
-    
+    all_roots = bus_timetable.objects.all()
+
     for o in all_roots:
-        got_s=False
-        
-        data = OrderedDict([ 
-                                 (o.st_1, o.t_1), 
-                                 (o.st_2, o.t_2),
-                                 (o.st_3, o.t_3),
-                                 (o.st_4, o.t_4),
-                                 (o.st_5, o.t_5),
-                                 (o.st_6, o.t_6),
-                                ]) 
-        
-        for i ,t in data.items():
-         
-            if str(i).lower()== str(q_s).lower():
-                t_s=t
-                got_s=True
-             
-            if str(i).lower()== str(q_d).lower() and got_s==True:
-                    context={
-                        "b_id":o.id,
-                        "b_name":o.bus_name,
-                        "s_time":t_s,
-                        "r_time":t
-                        }
-                    d.append(context)
-                    got_s=False
-                    break 
+        got_s = False
 
+        data = OrderedDict([
+            (o.st_1, o.t_1),
+            (o.st_2, o.t_2),
+            (o.st_3, o.t_3),
+            (o.st_4, o.t_4),
+            (o.st_5, o.t_5),
+            (o.st_6, o.t_6),
+        ])
 
-    d={"d":d,
-    "s":q_s.upper(),
-        "r":q_d.upper(),
-    }
+        for i, t in data.items():
 
-   
-    
-    return render(request,"show_table.html",d)
+            if str(i).lower() == str(q_s).lower():
+                t_s = t
+                got_s = True
+
+            if str(i).lower() == str(q_d).lower() and got_s:
+                context = {
+                    "b_id": o.id,
+                    "b_name": o.bus_name,
+                    "s_time": t_s,
+                    "r_time": t
+                }
+                d.append(context)
+                got_s = False
+                break
+
+    d = {"d": d,
+         "s": q_s.upper(),
+         "r": q_d.upper(),
+         }
+
+    return render(request, "show_table.html", d)
 # Create your views here.
 
-def bus_details(request,id,*args,**kargs):
+
+def bus_details(request, id, *args, **kargs):
     try:
-        bus=bus_timetable.objects.get(id=id)
+        bus = bus_timetable.objects.get(id=id)
     except bus_timetable.DoesNotExist:
         raise Http404
-    context={"bus":bus} 
-    return render(request,"bus_details.html",context)
+    context = {"bus": bus}
+    return render(request, "bus_details.html", context)
+
 
 def redirect_to_login(request):
     return render(request, 'login.html')
+
 
 def admin_login(request):
     if request.method == 'POST':
@@ -92,6 +92,7 @@ def admin_login(request):
             return HttpResponse('Login Failed')
     else:
         return render(request, 'login.html')
+
 
 @login_required
 def add_data(request):
@@ -107,13 +108,15 @@ def add_data(request):
         station3 = request.POST.get('station3')
         time3 = request.POST.get('time3')
 
-        data = bus_timetable(bus_name=bus_name, bus_no=bus_number, st_1=station1, t_1=time1, st_2=station2, t_2=time2, st_3=station3, t_3=time3)
+        data = bus_timetable(bus_name=bus_name, bus_no=bus_number, st_1=station1,
+                             t_1=time1, st_2=station2, t_2=time2, st_3=station3, t_3=time3)
         data.save()
 
         context['success_message'] = 'Data Added'
         return render(request, 'add_data.html', context)
 
     return render(request, 'add_data.html', context)
+
 
 @login_required
 def admin_logout(request):
